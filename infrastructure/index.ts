6acaecf5d3510303658d5e083b1466f1f14150ce
Navaneeth <32010211+navaneeth-dev@users.noreply.github.com>
2025-07-29 23:40:17 +0530

    feat: talos worker

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,13 +1,13 @@
 import * as talos from "@pulumi/talos";
 import * as pulumi from "@pulumi/pulumi";
-import * as k8s from "@pulumi/kubernetes";
 
 const CONTROLPLANE_IP = "192.168.2.131";
+const WORKER_IPS = ["192.168.2.138"];
 const CLUSTERNAME = "homelab-cluster";
 
 const secrets = new talos.MachineSecrets("this", {});
 
-const machineConfig = secrets.machineSecrets.apply(machineSecrets =>
+const controlplaneMachineConfig = secrets.machineSecrets.apply(machineSecrets =>
     talos.getMachineConfiguration({
         clusterName: CLUSTERNAME,
         machineType: "controlplane",
@@ -15,16 +15,38 @@ const machineConfig = secrets.machineSecrets.apply(machineSecrets =>
         machineSecrets,
     })
 );
+const workerMachineConfig = secrets.machineSecrets.apply(machineSecrets =>
+    talos.getMachineConfiguration({
+        clusterName: CLUSTERNAME,
+        machineType: "worker",
+        clusterEndpoint: `https://${CONTROLPLANE_IP}:6443`,
+        machineSecrets,
+    })
+);
 
-const clientConfig = secrets.clientConfiguration.apply(clientConfiguration =>
+const talosClientConfig = secrets.clientConfiguration.apply(clientConfiguration =>
     talos.getClientConfiguration({
         clusterName: CLUSTERNAME,
         clientConfiguration: clientConfiguration,
-        nodes: [CONTROLPLANE_IP],
+        nodes: [CONTROLPLANE_IP, ...WORKER_IPS],
     }).then(r => r.clientConfiguration)
 );
 
-const applyConfig = pulumi.all([clientConfig, machineConfig]).apply(([clientConfiguration, machineConfiguration]) =>
+const workerApplyConfig = pulumi.all([talosClientConfig, workerMachineConfig]).apply(([clientConfiguration, machineConfiguration]) =>
+    new talos.MachineConfigurationApply("worker", {
+        clientConfiguration: clientConfiguration,
+        machineConfigurationInput: machineConfiguration.machineConfiguration,
+        node: WORKER_IPS[0],
+        configPatches: [`machine:
+    install:
+        disk: /dev/sda
+        image: factory.talos.dev/installer/ce4c980550dd2ab1b17bbf2b08801c7eb59418eafe8f279833297925d67c7515:v1.10.5
+        wipe: false
+`
+        ],
+    })
+);
+const applyConfig = pulumi.all([talosClientConfig, controlplaneMachineConfig]).apply(([clientConfiguration, machineConfiguration]) =>
     new talos.MachineConfigurationApply("this", {
         clientConfiguration: clientConfiguration,
         machineConfigurationInput: machineConfiguration.machineConfiguration,
@@ -41,12 +63,12 @@ const applyConfig = pulumi.all([clientConfig, machineConfig]).apply(([clientConf
 
 const bootstrap = new talos.MachineBootstrap("this", {
     node: CONTROLPLANE_IP,
-    clientConfiguration: clientConfig,
-}, { dependsOn: [applyConfig] });
+    clientConfiguration: talosClientConfig,
+}, { dependsOn: [applyConfig, workerApplyConfig] });
 
 const kubeconfig = new talos.ClusterKubeconfig("this", {
     node: CONTROLPLANE_IP,
-    clientConfiguration: clientConfig,
+    clientConfiguration: talosClientConfig,
 }, { dependsOn: [bootstrap] });
 
 export const kubeconfigRaw = kubeconfig.kubeconfigRaw;
