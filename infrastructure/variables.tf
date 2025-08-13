8816025763800d020c0abb261ca59351286ba372
Navaneeth <32010211+navaneeth-dev@users.noreply.github.com>
2025-08-13 21:30:24 +0530

    feat: initial terraform migration

---
+++ infrastructure/variables.tf
@@ -0,0 +1,45 @@
+variable "cluster_name" {
+  description = "A name to provide for the Talos cluster"
+  type        = string
+}
+
+variable "cluster_endpoint" {
+  description = "The endpoint for the Talos cluster"
+  type        = string
+}
+
+variable "node_data" {
+  description = "A map of node data"
+  type = object({
+    controlplanes = map(object({
+      install_disk = string
+      hostname     = optional(string)
+    }))
+    workers = map(object({
+      install_disk = string
+      hostname     = optional(string)
+    }))
+  })
+  default = {
+    controlplanes = {
+      "192.168.2.131" = {
+        install_disk = "/dev/sda"
+        hostname     = "talos1"
+      },
+      "192.168.2.128" = {
+        install_disk = "/dev/sda"
+        hostname     = "talos2"
+      },
+    }
+    workers = {
+      "192.168.2.138" = {
+        install_disk = "/dev/sda"
+        hostname     = "talos-worker2"
+      },
+      "192.168.2.221" = {
+        install_disk = "/dev/sda"
+        hostname     = "talos-worker2"
+      }
+    }
+  }
+}
