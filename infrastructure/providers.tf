0dcfc9500b5126a06f02b2569cb49cd1bfe9768d
Navaneeth <32010211+navaneeth-dev@users.noreply.github.com>
2025-08-14 14:25:02 +0530

    feat: added s3 backend to terraform

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "0.9.0-alpha.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "rize-tfstate"
+    key          = "fluxcd-homelab/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+  }
 }
 
 provider "talos" {}
