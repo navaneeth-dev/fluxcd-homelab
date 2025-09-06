99c891f210d082983badc28e27d6e6d6bf72b9af
Navaneeth <32010211+navaneeth-dev@users.noreply.github.com>
2025-09-06 19:11:32 +0530

    feat: decrease mem limit

---
+++ infrastructure/proxmox/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.83.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "rize-tfstate"
+    key          = "homelab-proxmox/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+  }
+}
+
+provider "proxmox" {}
